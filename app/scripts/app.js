3c810e16de918c3612d0364cdfde8cdb19a2edfe
Patrick Douglas <pcdouglas94@gmail.com>
2014-08-04 19:04:06 -0700

    Added angular-bootstrap-slider for use in todo list progress.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngSanitize',
     'ngTouch',
 	'ui.router',
-	'ui.bootstrap'
+	'ui.bootstrap',
+	'ui.bootstrap-slider'
   ])
   
   .config(['$urlRouterProvider', function ($urlRouterProvider) {
